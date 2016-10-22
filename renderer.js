8ff7b3c735b446c19b3ca8ea674db13e8752c96d
Priit Haamer <priit@haamer.ee>
2016-10-22 13:42:19 +0300

    Switch from ACE to CodeMirror

--- renderer.js
+++ renderer.js
@@ -2,24 +2,39 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-// const ace = require('ace-builds/src/ace');
+const CodeMirror = require('codemirror');
+const htmlmixedmode = require('codemirror/mode/htmlmixed/htmlmixed');
+const cssmode = require('codemirror/mode/css/css');
+const jsmode = require('codemirror/mode/javascript/javascript');
 
-const htmlEditor = ace.edit(document.querySelector('.html-editor'));
-htmlEditor.session.setMode('ace/mode/html');
-htmlEditor.session.on('change', (event, session) => {
-  updateContents('html', session.getValue());
+const editorOptions = {
+  indentUnit: 2,
+  smartIndent: true,
+  tabSize: 2,
+  lineNumbers: true
+}
+
+const htmlEditor = CodeMirror(document.querySelector('.html-editor'), Object.assign({}, editorOptions, {
+  value: 'Hello!',
+  mode: 'htmlmixed',
+}));
+htmlEditor.on('change', (editor, change) => {
+  updateContents('html', editor.doc.getValue());
 });
 
-const cssEditor = ace.edit(document.querySelector('.css-editor'));
-cssEditor.session.setMode('ace/mode/css');
-cssEditor.session.on('change', (event, session) => {
-  updateContents('css', session.getValue());
+const cssEditor = CodeMirror(document.querySelector('.css-editor'), Object.assign({}, editorOptions, {
+  value: 'body { color: red; }',
+  mode: 'css',
+}));
+cssEditor.on('change', (editor, change) => {
+  updateContents('css', editor.doc.getValue());
 });
 
-const jsEditor = ace.edit(document.querySelector('.js-editor'));
-jsEditor.session.setMode('ace/mode/javascript');
-jsEditor.session.on('change', (event, session) => {
-  updateContents('javascript', session.getValue());
+const jsEditor = CodeMirror(document.querySelector('.js-editor'), Object.assign({}, editorOptions, {
+  mode: 'javascript',
+}));
+jsEditor.on('change', (editor, change) => {
+  updateContents('javascript', editor.doc.getValue());
 });
 
 const preview = document.querySelector('#preview');
@@ -30,4 +45,10 @@ const updateContents = (kind, contents) => {
 
 preview.addEventListener('console-message', (e) => {
   console.log('Guest page logged a message:', e.message);
+});
+
+preview.addEventListener('dom-ready', () => {
+  updateContents('html', htmlEditor.doc.getValue());
+  updateContents('css', cssEditor.doc.getValue());
+  updateContents('javascript', jsEditor.doc.getValue());
 });
