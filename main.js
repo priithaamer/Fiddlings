1a8a5703b204053e4777367081fa61b5d695bc87
Priit Haamer <priit@haamer.ee>
2016-11-26 15:36:36 +0200

    Support multiple windows

--- main.js
+++ main.js
@@ -6,24 +6,28 @@ const app = electron.app;
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow;
 
-// Keep a global reference of the window object, if you don't, the window will
+// Keep a global reference of the windows, if you don't, the windows will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+let windows = [];
 
 function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1000, height: 700});
+  const window = new BrowserWindow({width: 1000, height: 700});
 
   // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  window.loadURL(`file://${__dirname}/index.html`);
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
+  window.on('closed', function() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null;
+    windows.splice(windows.indexOf(window), 1);
   });
+
+  windows.push(window);
+
+  return window;
 }
 
 function createMenu() {
@@ -31,7 +35,13 @@ function createMenu() {
     {
       label: 'File',
       submenu: [
-        // {label: 'New File'},
+        {
+          label: 'New File',
+          accelerator: 'CmdOrCtrl+Shift+N',
+          click() {
+            createWindow();
+          }
+        },
         {
           label: 'Open...',
           accelerator: 'CmdOrCtrl+O',
@@ -42,7 +52,8 @@ function createMenu() {
                 return;
               }
 
-              focusedWindow.webContents.send('file-open', fileNames[0]);
+              const window = createWindow();
+              window.webContents.send('file-open', fileNames[0]);
             });
           }
         },
@@ -82,28 +93,6 @@ function createMenu() {
     {
       label: 'View',
       submenu: [
-        {
-          label: 'Reload',
-          accelerator: 'CmdOrCtrl+R',
-          click(item, focusedWindow) {
-            if (focusedWindow) {
-              focusedWindow.reload();
-            }
-          }
-        },
-        {
-          label: 'Toggle Developer Tools',
-          accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
-          click(item, focusedWindow) {
-            if (focusedWindow) {
-              focusedWindow.webContents.toggleDevTools();
-            }
-          }
-        },
-        {role: 'resetzoom'},
-        {role: 'zoomin'},
-        {role: 'zoomout'},
-        {type: 'separator'},
         {role: 'togglefullscreen'}
       ]
     },
@@ -120,7 +109,8 @@ function createMenu() {
         {
           label: 'Learn More',
           click() {
-            require('electron').shell.openExternal('http://electron.atom.io');
+            // TODO: Link to fiddlings documentation site
+            require('electron').shell.openExternal('https://github.com/priithaamer/Fiddlings');
           }
         }
       ]
@@ -150,13 +140,40 @@ function createMenu() {
       {label: 'Speech', submenu: [{role: 'startspeaking'}, {role: 'stopspeaking'}]}
     );
     // Window menu.
-    template[3].submenu = [
+    template[4].submenu = [
       {label: 'Close', accelerator: 'CmdOrCtrl+W', role: 'close'},
       {label: 'Minimize', accelerator: 'CmdOrCtrl+M', role: 'minimize'},
       {label: 'Zoom', role: 'zoom'},
       {type: 'separator'},
       {label: 'Bring All to Front', role: 'front'}
     ];
+  }
+
+  if (process.env.NODE_ENV === 'development') {
+    template[3].submenu.push(
+      {type: 'separator'},
+      {
+        label: 'Reload',
+        accelerator: 'CmdOrCtrl+R',
+        click(item, focusedWindow) {
+          if (focusedWindow) {
+            focusedWindow.reload();
+          }
+        }
+      },
+      {
+        label: 'Toggle Developer Tools',
+        accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
+        click(item, focusedWindow) {
+          if (focusedWindow) {
+            focusedWindow.webContents.toggleDevTools();
+          }
+        }
+      },
+      {role: 'resetzoom'},
+      {role: 'zoomin'},
+      {role: 'zoomout'}
+    );
   }
 
   const menu = Menu.buildFromTemplate(template);
@@ -181,7 +198,7 @@ app.on('window-all-closed', function() {
 app.on('activate', function() {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
+  if (windows.length === 0) {
     createWindow();
   };
 });
